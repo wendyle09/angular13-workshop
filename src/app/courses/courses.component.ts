760d1a77a0c4deffd868d6496386e175ee1bb7e4
wendyle <wendyle09@gmail.com>
2023-03-20 14:49:53 -0700

    render list of courses and show course details on click

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,22 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  // render courses in a list
+  // select a course from list
+  // render selected course
+
   courses = [
     {
       id: 1,
       title: 'Angular 13 Fundamentals',
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
-      favorite: true
-    }
+      favorite: true,
+    },
+    {
+      id: 2,
+      title: 'Angular 13 Advance',
+      description: 'Delve deeper into Angular 13',
+      percentComplete: 0,
+      favorite: false,
+    },
   ];
+  selectedCourse = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectCourse(course) {
+    this.selectedCourse = course;
   }
-
 }
